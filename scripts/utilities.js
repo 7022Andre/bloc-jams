e1ee1db3c61a60a47ecb0c15c6f516154b92284f
7022Andre <andre.mueller@zoho.com>
2017-02-28 13:06:09 -0800

    Assignment 9 update

--- scripts/utilities.js
+++ scripts/utilities.js
@@ -1,11 +1,5 @@
-function forEach(array) {
-    var revealPoint = function(index) {
-        array[index].style.opacity = 1;
-        array[index].style.transform = "scaleX(1) translateY(0)";
-        array[index].style.msTransform = "scaleX(1) translateY(0)";
-        array[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
+function forEach(array, callback) {
     for (var i = 0; i < array.length; i++) {
-        revealPoint(i);
+        callback(array[i]);
     }
 }
